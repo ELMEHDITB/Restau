d858f10425f8e7e1e84ca6babd7775e20b3abf8a
Taleb Elmehdi <talebelmehdi1@gmail.com>
2018-06-17 22:40:21 +0000

    Sucess

---
+++ restaubackend/src/main/java/net/ensa/restaubackend/App.java
@@ -0,0 +1,13 @@
+package net.ensa.restaubackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
